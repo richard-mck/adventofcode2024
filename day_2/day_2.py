fc89dd9b6e82c6d31b25ec04902ddbe768f6a91b
Richard McKenzie <rmckenzie@lululemon.com>
2024-12-06 15:16:42 -0800

    refactor: move row safety check to separate function

--- day_2/day_2.py
+++ day_2/day_2.py
@@ -5,6 +5,17 @@ def get_sorted_and_reversed(row: list[int]) -> (list[int], list[int]):
     sorted_row.sort()
     return sorted_row, list(reversed(sorted_row))
 
+def check_if_row_is_safe(row: list[int]) -> bool:
+    for i in range(1, len(row)):
+        diff = abs(row[i] - row[i - 1])
+        print(f"{diff} = {row[i]} - {row[i - 1]}")
+        # If the values are within 1-3 AND not the same, we're safe
+        if diff <= 3 and diff != 0:
+            continue
+        else:
+            return False
+    return True
+
 if __name__ == '__main__':
     # Day 2, part 1
     data = get_real_data(False)
@@ -19,14 +30,7 @@ if __name__ == '__main__':
         # If a report is already sorted or reverse sorted correctly, we only need to check the differences between the values
         if row == sorted_row or row == reverse_row:
             safe_reports.append(row)
-            for i in range(1, len(row)):
-                diff = abs(row[i] - row[i - 1])
-                print(f"{diff} = {row[i]} - {row[i - 1]}")
-                # If the values are within 1-3 AND not the same, we're safe
-                if diff <= 3 and diff != 0:
-                    continue
-                else:
-                    safe_reports.pop()
-                    break
+            if not check_if_row_is_safe(row):
+               safe_reports.pop()
     print(safe_reports)
     print(len(safe_reports))
