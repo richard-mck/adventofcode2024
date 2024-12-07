b043d77948c49e9b4243de40092012ce06b4b653
Richard McKenzie <rmckenzie@lululemon.com>
2024-12-06 16:18:07 -0800

    refactor: move difference check call to ordering func for simplicity

--- day_2/day_2.py
+++ day_2/day_2.py
@@ -4,13 +4,12 @@ def check_get_sorted_and_reversed(row: list[int]) -> bool:
     sorted_row = row.copy()
     sorted_row.sort()
     if row == sorted_row or row == list(reversed(sorted_row)):
-        return True
+        return check_if_row_is_safe(row)
     return False
 
 def check_if_row_is_safe(row: list[int]) -> bool:
     for i in range(1, len(row)):
         diff = abs(row[i] - row[i - 1])
-        print(f"{diff} = {row[i]} - {row[i - 1]}")
         # If the values are within 1-3 AND not the same, we're safe
         if diff <= 3 and diff != 0:
             continue
@@ -30,7 +29,5 @@ if __name__ == '__main__':
         # If a report is already sorted or reverse sorted correctly, we only need to check the differences between the values
         if check_get_sorted_and_reversed(row):
             safe_reports.append(row)
-            if not check_if_row_is_safe(row):
-                safe_reports.pop()
     print(safe_reports)
     print(len(safe_reports))
