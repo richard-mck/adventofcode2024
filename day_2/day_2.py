fd64190b43c24ef98fa5eab30a38ac1083fb65eb
Richard McKenzie <rmckenzie@lululemon.com>
2024-12-06 16:19:14 -0800

    feat: working solution for day 2 part 2

--- day_2/day_2.py
+++ day_2/day_2.py
@@ -31,3 +31,22 @@ if __name__ == '__main__':
             safe_reports.append(row)
     print(safe_reports)
     print(len(safe_reports))
+
+    # Day 2, Part 2
+    reports_with_damper = []
+    for row in data:
+        # Append known safe rows
+        if row in safe_reports:
+            reports_with_damper.append(row)
+            continue
+        # Check each iteration of the row with one value removed
+        for i in range(len(row)):
+            pop_row = row.copy()
+            pop_row.pop(i)
+            if check_get_sorted_and_reversed(pop_row):
+                print(f"Row is safe: {pop_row} - original row: {row}")
+                reports_with_damper.append(row)
+                # If a row is safe with a single value removed, no further checks are needed
+                break
+
+    print(f"Number of safe reports: {len(reports_with_damper)}")
