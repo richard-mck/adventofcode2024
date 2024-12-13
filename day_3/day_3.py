67920ab6519cf3de75fe2e70aa6e1d62748fdc8b
Richard McKenzie <rmckenzie@lululemon.com>
2024-12-13 12:33:35 -0800

    feat: add function to break multipliers within brackets

--- day_3/day_3.py
+++ day_3/day_3.py
@@ -1,3 +1,8 @@
+def split_tuple_between_brackets(values: str) -> list[str]:
+    first_bracket = values.index("(")
+    second_bracket = values.index(")")
+    return values[first_bracket + 1:second_bracket].split(",")
+
 if __name__ == '__main__':
     with open('example.txt') as f:
         data = f.readlines()
